31b35b6a7ccd4834d23d9fa82f9c95b7b1d800f7
Adam <SaturnsVoid@users.noreply.github.com>
2015-10-07 13:40:40 -0400

    Initial basics

--- GoBot.php
+++ GoBot.php
@@ -1,2 +1,17 @@
+<?php
+define("SQLHOST", "127.0.0.1");
+define("SQLNAME", "test");
+define("SQLUSER", "root");
+define("SQLPASS", "");
 
-QUxMfDF8aHR0cDovL3d3dy5nb29nbGUuY29tfFY=
+if (isset($_GET['get']))
+	//Get get, Check SQL for match
+	//if match found check for commands
+	//if match not found add to known bots
+  echo "test";
+elseif (isset($_GET['test']))
+	echo "ok"; //Send back information on last command
+else
+	echo "<h1>404 Page not found</h1>";
+
+?>
